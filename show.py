724000b1655c1c9f299a92d138c481fbad158e88
takeshi <id3.gaku@gmail.com>
2022-12-17 17:31:17 +0900

    add today

--- show.py
+++ show.py
@@ -1,6 +1,7 @@
 import random
 
 with open("./hyakunin.txt", encoding="utf-8") as f:
-    wakes = [s.strip() for s in f.readlines()]
+    wakas = [s.strip() for s in f.readlines()]
 
-print(wakas[random.randrange(len(wakes))])
+print(”今日の一句” + wakas[random.randrange(len(wakes))])
+
